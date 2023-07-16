e810e253acdb1ffcb677bb98a4f55aa6c40f848c
Osaid Mohammad <osho1525@gmail.com>
2023-07-16 18:51:38 -0500

    done

--- portfolio_web/src/app/CardContainer.jsx
+++ portfolio_web/src/app/CardContainer.jsx
@@ -10,9 +10,12 @@ const items = cards.map((card)=>{
 })
 const CardContainer = () => {
     return(
-        <div className="mt-32 flex justify-center items-center flex-wrap gap-10 " id = "project">
-            {items}
-        </div> 
+        <div className = "mt-24 mb-24" id = "project"> 
+            <div className = "text-4xl font-bold text-center">Projects</div>
+            <div className="pt-10 flex justify-center items-center flex-wrap gap-10 ">
+                {items}
+            </div> 
+        </div>
     )
 }
 export default dynamic(() => Promise.resolve(CardContainer), { ssr: false });
--- portfolio_web/src/app/header.jsx
+++ portfolio_web/src/app/header.jsx
@@ -1,9 +1,11 @@
 import dynamic from "next/dynamic"
+import Image from "next/image"
 const Header = () => {
     return(
-        <div className="flex items-center flex-col mt-20 flex-shrink text-center">
+        <div className="flex items-center flex-col mt-20 flex-shrink text-center" id = "about">
             <div className="font-bold text-xl text-center">Welcome to my Website!</div>   
             <div>I am a aspiring software engineer and current student and the University of Houston</div>
+            <Image src = "/banner_photo.jpg" width = {500} height = {50} className = "pt-[30px] rounded-2xl"></Image>
         </div>
     )
 }
